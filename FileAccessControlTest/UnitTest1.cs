2853eb130e26c8f89b28f28aa186cc92578d84d2
nicolegarcia16 <nicole.futch16@gmail.com>
2017-02-23 16:55:02 -0500

    added more unit tests, created new methods

--- FileAccessControlTest/UnitTest1.cs
+++ FileAccessControlTest/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FileAccessControl;
+using System.Security.AccessControl;
 
 namespace FileAccessControlTest
 {
@@ -14,6 +15,55 @@ namespace FileAccessControlTest
             Form1 frm = new Form1();
             bool isValid = frm.validateFilename(filename);
             Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        public void TestGetAccessControlString()
+        {
+            string filename = "C:\\Users\\nicol\\Documents\\Graduate Classes\\SSE 554\\Project 1\\Project1.docx";
+            ACLView frm = new ACLView(filename);
+            FileSystemAccessRule rule = new FileSystemAccessRule("ident", FileSystemRights.FullControl, AccessControlType.Allow);
+            string accessControlString = frm.getAccessControlString(rule);
+            Assert.IsTrue(accessControlString != null && accessControlString.Length > 0);
+        }
+
+        [TestMethod]
+        public void TestGetSystemRightsString()
+        {
+            string filename = "C:\\Users\\nicol\\Documents\\Graduate Classes\\SSE 554\\Project 1\\Project1.docx";
+            ACLView frm = new ACLView(filename);
+            FileSystemAccessRule rule = new FileSystemAccessRule("ident", FileSystemRights.FullControl, AccessControlType.Allow);
+            string systemRightsString = frm.getSystemRightsString(rule);
+            Assert.IsTrue(systemRightsString != null && systemRightsString.Length > 0);
+        }
+
+        [TestMethod]
+        public void TestGetIdentityReferenceString()
+        {
+            string filename = "C:\\Users\\nicol\\Documents\\Graduate Classes\\SSE 554\\Project 1\\Project1.docx";
+            ACLView frm = new ACLView(filename);
+            FileSystemAccessRule rule = new FileSystemAccessRule("ident", FileSystemRights.FullControl, AccessControlType.Allow);
+            string identityReferenceString = frm.getIdentityReferenceString(rule);
+            Assert.IsTrue(identityReferenceString != null && identityReferenceString.Length > 0);
+        }
+
+        [TestMethod]
+        public void TestGetInheritedRulesString()
+        {
+            string filename = "C:\\Users\\nicol\\Documents\\Graduate Classes\\SSE 554\\Project 1\\Project1.docx";
+            ACLView frm = new ACLView(filename);
+            FileSystemAccessRule rule = new FileSystemAccessRule("ident", FileSystemRights.FullControl, AccessControlType.Allow);
+            string inheritedRulesString = frm.getInheritedRulesString(rule);
+            Assert.IsTrue(inheritedRulesString != null && inheritedRulesString.Length > 0);
+        }
+
+        [TestMethod]
+        public void TestGetAuthorizationRules()
+        {
+            string filename = "C:\\Users\\nicol\\Documents\\Graduate Classes\\SSE 554\\Project 1\\Project1.docx";
+            ACLView frm = new ACLView(filename);
+            AuthorizationRuleCollection rules = frm.getAuthorizationRules(filename);
+            Assert.IsTrue(rules.Count > 0);
         }
     }
 }
